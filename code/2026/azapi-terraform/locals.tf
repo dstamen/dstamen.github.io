425e3822f9c343c8e7a9d5601aeba2c5407ccd44
David Stamen <dstamen@gmail.com>
2026-09-01 13:09:46 -0700

    Publishing to GitHub

--- code/2026/azapi-terraform/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  monitor_api_version = "2023-04-03"
-  grafana_api_version = "2023-09-01"
-  rg_id               = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group}"
-}
